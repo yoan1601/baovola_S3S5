ba54bafd7d264602d2052dd289a2820bbba7d003
Mikaraban <Tokytrabanoson@gmail.com>
2023-12-08 10:05:01 +0300

    tsisy modif
    tsisy

---
+++ java/testjava/Func.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testjava;
+public class Func {
+    public int plus4(int i){
+        return i+4;
+    }
+}
